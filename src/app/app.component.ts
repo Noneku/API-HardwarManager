82459bc01d491bbd49ea618ab72058bef9f659ae
Noneku <dev.nassim.pro@gmail.com>
2024-06-20 11:32:01 +0200

    Uninstall PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,17 @@
 
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { TabMenuComponent } from "./tab-menu/tab-menu.component";
-import { FilterMatchMode, PrimeNGConfig } from "primeng/api";
 import {TabStatesServersComponent} from "./tab-states-servers/tab-states-servers.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TabStatesServersComponent, TabMenuComponent],
+  imports: [RouterOutlet, TabStatesServersComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   title : String = "HardwareManager";
-  constructor(private primengConfig: PrimeNGConfig) {}
 
-  ngOnInit() {
-    this.primengConfig.csp.set({ nonce: '...' });
-    this.primengConfig.ripple = true;
-
-    this.primengConfig.zIndex = {
-      modal: 1100,    // dialog, sidebar
-      overlay: 1000,  // dropdown, overlaypanel
-      menu: 1000,     // overlay menus
-      tooltip: 1100   // tooltip
-    };
-
-    this.primengConfig.filterMatchModeOptions = {
-      text: [FilterMatchMode.STARTS_WITH, FilterMatchMode.CONTAINS, FilterMatchMode.NOT_CONTAINS, FilterMatchMode.ENDS_WITH, FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS],
-      numeric: [FilterMatchMode.EQUALS, FilterMatchMode.NOT_EQUALS, FilterMatchMode.LESS_THAN, FilterMatchMode.LESS_THAN_OR_EQUAL_TO, FilterMatchMode.GREATER_THAN, FilterMatchMode.GREATER_THAN_OR_EQUAL_TO],
-      date: [FilterMatchMode.DATE_IS, FilterMatchMode.DATE_IS_NOT, FilterMatchMode.DATE_BEFORE, FilterMatchMode.DATE_AFTER]
-    };
-  }
 }
